8eed673d723edf059912fb3b16f4891f12437e96
Rdowafun <jabrooster@gmail.com>
2023-01-25 21:49:25 +0500

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let length = str.length
+  for (let i = 0; i < length; i++) {
+    bracketsConfig.map(e => {
+      const config = e.join('')
+      if (str.includes(config)) str = str.replace(config, "")
+      return config;
+    })
+  }
+  return str.length ? false : true
 }
